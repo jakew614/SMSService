2444410f67e3f16e3198c2fe99d39e959239559e
JakeW <X00170642@myTUDublin.ie>
2023-02-26 11:46:18 +0000

    Add project files.

---
+++ SMSService/Controllers/SMSController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SMSService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSController : ControllerBase
+    {
+        // GET: api/<SMSController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SMSController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SMSController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SMSController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SMSController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SMSService/Models/CreateSMS.cs
@@ -0,0 +1,51 @@
+﻿namespace SMSService.Models
+{
+    public class CreateSMS
+    {
+        private string smstext;
+
+        public string SMSText
+        {
+            get { return smstext; }
+            set
+            {
+                if (smstext.Length > 140)
+                {
+                    throw new ArgumentException("SMS must be less than 140 characters");
+                }
+                smstext = value;
+            }
+        }
+
+        private int sendermobile;
+
+        public int SenderMobile
+        {
+            get { return sendermobile; }
+            set
+            {
+                if (string.IsNullOrEmpty(SenderMobile.ToString()))
+                {
+                    throw new ArgumentException("Senders number cannot be emptied;");
+                }
+                sendermobile = value;
+            }
+        }
+
+
+        private int receivermobile;
+
+        public int ReceiverMobile
+        {
+            get { return receivermobile; }
+            set
+            {
+                if (string.IsNullOrEmpty(ReceiverMobile.ToString()))
+                {
+                    throw new ArgumentException("Senders number cannot be emptied;");
+                }
+                receivermobile = value;
+            }
+        }
+    }
+}
